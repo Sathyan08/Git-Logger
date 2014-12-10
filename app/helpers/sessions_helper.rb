ecf48fad05d970f99e407862db25ce0f96b5ac2f
Sathyan Mathai <Sathyan.Mathai@gmail.com>
2014-12-10 16:56:24 -0500

    Altered header.html.haml to prevent errors and added methods to session helper for current user, user_signed_in? and set_current_user

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,2 +1,13 @@
-module SessionsHelper
+module SessionHelper
+  def user_signed_in?
+    !current_user.nil?
+  end
+
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
+
+  def set_current_user(user)
+    @current_user = user
+  end
 end
